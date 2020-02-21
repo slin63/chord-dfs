51a8dc3ab6b95e51fbdff8adc9395c8a346b7292
Shean Lin <shean.lin2018@gmail.com>
2020-02-21 14:44:44 -0500

    first commit

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+
+	"./node"
+)
+
+const logf = "o.log"
+
+func main() {
+	isIntroducer, err := strconv.ParseBool(os.Getenv("INTRODUCER"))
+	if err != nil {
+		log.Fatal("INTRODUCER not set in this environment")
+	}
+	flag.Parse()
+
+	node.Live(isIntroducer, logf)
+}
