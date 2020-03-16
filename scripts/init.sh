df8f8f8ab46a84b69073529ce59dfd865585dc76
Shean Lin <shean.lin2018@gmail.com>
2020-03-16 17:43:13 -0500

    now launches entire ecosystem on docker compose. need to find a way to re-clone repositories when they've changed, because currently if you push a new change to repository, docker will just use the cached version

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 trap 'kill $(jobs -p)' TERM
 
-./dfs &
-./member &
+CONFIG=$GODIR/chord-dfs/config.json ./dfs &
+CONFIG=$GODIR/chord-failure-detector/config.json ./member &
+if [ $ALL = 1 ]
+then
+    CONFIG=$GODIR/raft-consensus/config.json ./raft &
+fi
 
 wait
